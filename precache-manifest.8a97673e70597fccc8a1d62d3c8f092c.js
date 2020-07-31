a7450990a3eac7304a1db752f9e58ea8ef7f52cf
MarikKV <kosaniakm@gmail.com>
2020-07-31 22:01:11 +0300

    Updates

---
+++ precache-manifest.8a97673e70597fccc8a1d62d3c8f092c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8a25df6446c2f5386fdd3ba2744454",
+    "url": "/Announcer/index.html"
+  },
+  {
+    "revision": "e355d44788d5122bea37",
+    "url": "/Announcer/static/css/main.806b7b51.chunk.css"
+  },
+  {
+    "revision": "1289df987fb1520e363b",
+    "url": "/Announcer/static/js/2.ba5db55b.chunk.js"
+  },
+  {
+    "revision": "3727fc7e8013775eca6a24be339a7d5f",
+    "url": "/Announcer/static/js/2.ba5db55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e355d44788d5122bea37",
+    "url": "/Announcer/static/js/main.d53c0627.chunk.js"
+  },
+  {
+    "revision": "ac35ad41d86c2e5666f0",
+    "url": "/Announcer/static/js/runtime-main.011bf7ad.js"
+  }
+]);
